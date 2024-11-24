4f3a5d17496283b9d11132e818ad197d0c3e2ab1
omjinLTS <omjin7g@gmail.com>
2024-11-25 06:00:39 +0900

    get current tab's url

--- pages/side-panel/src/pages/AnalysisPage.tsx
+++ pages/side-panel/src/pages/AnalysisPage.tsx
@@ -1,3 +1,36 @@
+import { Button } from '@/components/ui/button';
+
+const BeforeAnalysis = () => {
+  return (
+    <p className="text-xl">
+      현재 페이지의 상품을 분석하려면
+      <br />
+      분석하기 버튼을 눌러 주세요!
+    </p>
+  );
+};
+
+const AnalysisButton = () => {
+  return (
+    <Button
+      onClick={async () => {
+        // get current main page's url
+        const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+        console.log(tab);
+        if (tab) {
+          alert(tab.url);
+        }
+      }}>
+      분석하기
+    </Button>
+  );
+};
+
 export default function AnalysisPage() {
-  return <div>분석페이지</div>;
+  return (
+    <div className="relative flex h-full flex-col items-center justify-center">
+      <AnalysisButton />
+      <BeforeAnalysis />
+    </div>
+  );
 }
